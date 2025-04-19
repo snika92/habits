5a543b747a56fa4a8ad105d7d7825e689befacc2
Veronika Surkova <surkova_nika92@mail.ru>
2025-04-19 15:29:18 +0300

    Fix HabitSerializer

--- habits/serializers.py
+++ habits/serializers.py
@@ -1,6 +1,8 @@
+from rest_framework.fields import SerializerMethodField
 from rest_framework.serializers import ModelSerializer
 
 from habits.models import Habit, Place
+from users.models import User
 
 
 class PlaceSerializer(ModelSerializer):
@@ -10,6 +12,11 @@ class PlaceSerializer(ModelSerializer):
 
 
 class HabitSerializer(ModelSerializer):
+    owner = SerializerMethodField()
+
+    def get_owner(self, habit):
+        return [owner.email for owner in User.objects.filter(habits=habit)]
+
     class Meta:
         model = Habit
         fields = "__all__"
